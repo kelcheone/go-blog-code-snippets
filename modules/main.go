b61d07c6fc65ea3027cea945bf0335f20d82f94a
kelche <kelcheone@gmail.com>
2023-02-04 20:51:57 +0300

    feat: go mod

---
+++ modules/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/labstack/echo/v4"
+
+
+func main() {
+  e := echo.New()
+  e.GET("/", func(c echo.Context) error {
+    return c.String(200, "I am a server!")
+  })
+  e.Logger.Fatal(e.Start(":1323"))
+}
